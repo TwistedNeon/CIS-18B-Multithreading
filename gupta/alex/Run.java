ab2ba44f60ca58bda31d8586d6565d433cc92f70
Alex Gupta <alkigu55@gmail.com>
2021-10-26 21:17:58 -0700

    Retesting 123

--- gupta/alex/Run.java
+++ gupta/alex/Run.java
@@ -4,11 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.Scanner;
-
 import animal.Animal;
-import animal.carnivore.Lion;
-import animal.herbivore.Giraffe;
 
 public class Run implements Runnable{
 	
@@ -18,15 +14,10 @@ public class Run implements Runnable{
 		private ArrayList<Animal> animalList;
 		private Thread thread;
 		
-	//User input variables
-		private Scanner reader = new Scanner(System.in);
-		private int input;
-		
 	//File variables
 		private File outputFile;
 		private BufferedWriter fileWriter;
-		private	File namesFile;
-		private Scanner fileReader;
+	
 		
 	//Constructor
 		Run(String threadName, String filePathWithName, ArrayList<Animal> animalList){
@@ -39,55 +30,30 @@ public class Run implements Runnable{
 	//Code to be executed
 		@Override
 		public void run() {
-			//Ask user for input
-				System.out.println("How many Giraffes?");
-				input = reader.nextInt();
-				createAnimal("Giraffe");
-				System.out.println("How many Lions?");
-				input = reader.nextInt();
-				createAnimal("Lion");
-				
-			//Write to file
-				try {
-					outputFile = new File("" + filePathWithName);
-					fileWriter = new BufferedWriter(new FileWriter(outputFile));
-					for(Animal thisAnimal : animalList) {
-						fileWriter.write("" + thisAnimal.getName() + " is a " + thisAnimal.getType() + "\n");
-						//System.out.println("" + thisAnimal.getName() + " is a " + thisAnimal.getType());
-					}
-						fileWriter.close();
-						System.out.println("Successfully outputted to file!\n" + outputFile.getAbsolutePath());
-					}catch(Exception e) {
-						System.out.println("Error writing to file!");
-					}
+			writeToFile();
 		}
-	
-	//Create specified animal from names file
-		public void createAnimal(String animal) {
+		
+	//Write to file
+		private void writeToFile() {
 			try {
-				namesFile = new File("names.txt");
-				fileReader = new Scanner(namesFile);
-				if(animal.equalsIgnoreCase("Giraffe")) {
-					for(int i = input; i > 0; i--) {
-						animalList.add(new Giraffe(fileReader.nextLine()));
-					}
+				outputFile = new File("" + filePathWithName);
+				fileWriter = new BufferedWriter(new FileWriter(outputFile));
+				for(Animal thisAnimal : animalList) {
+					fileWriter.write("" + thisAnimal.getName() + " is a " + thisAnimal.getType() + "\n");
+					//System.out.println("" + thisAnimal.getName() + " is a " + thisAnimal.getType());
 				}
-				else if(animal.equalsIgnoreCase("Lion")) {
-					for(int i = input; i > 0; i--) {
-						animalList.add(new Lion(fileReader.nextLine()));
-					}
-				}
-				fileReader.close();
-			}catch(Exception e) {
-				System.out.println("Error creating an animal!");
+					fileWriter.close();
+					System.out.println("Successfully outputted to file!\n" + outputFile.getAbsolutePath());
+				}catch(Exception e) {
+					System.out.println("Error writing to file!");
 			}
 		}
 	
 	//Static factory method
 		public static Run createAndStart(String threadName, String filePathWithName, ArrayList<Animal> animalList) {
-			Run returnRun = new Run(threadName, filePathWithName, animalList);
-			returnRun.thread.start();
-			return returnRun;
+			Run myRun = new Run(threadName, filePathWithName, animalList);
+			myRun.thread.start();
+			return myRun;
 		}
 	
 	//Allow Main class to join the threads
